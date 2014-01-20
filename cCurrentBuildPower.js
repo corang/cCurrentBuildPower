b745487b346e58d4f78c3bf225e88bdb7ce69f72
corang <jordanmc12@verizon.net>
2014-01-20 10:18:47 -0500

    Start Defining Variables

--- cCurrentBuildPower.js
+++ cCurrentBuildPower.js
@@ -1,1 +1,29 @@
 //WIP
+
+//All data for units and buildings comes from http://www.nanodesu.info/pa-db/59549/
+//Defining Metal Outputs for Fabricators and Factories
+var fabBotMetalOutput = 10
+var FabBotCombatMetalOutput = 15
+var fabVehicleMetalOutput = 10
+var fabAirMetalOutput = 6
+var fabShipMetalOutput = 12
+var fabAdvBotMetalOutput = 30
+var fabAdvCombatBotMetalOutput = 45
+var fabAdvAirMetalOutput = 18
+var commanderMetalOutput = 30
+var fabAdvShipMetalOutput = 36
+var fabOrbitalMetalOutput = 40
+/* Done Units
+
+Start Factories */
+var facVehicleMetalOutput = 15
+var facAdvVehicleMetalOutput = 45
+var facBotMetalOutput = 12
+var facAdvBotMetalOutput = 36
+var facAirMetalOutput = 9
+var facAdvAirMetalOutput = 27
+var facNavalMetalOutput = 18
+var facAdvNavalMetalOutput = 54
+var facOrbitalMetalOutput = 20
+var facNukeMetalOutput = 45
+var facAntiNukeMetalOutput = 36
